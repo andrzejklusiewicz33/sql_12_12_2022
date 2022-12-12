9e7353b41d0a7914e508a4743471684acb93c23e
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-12 12:57:09 +0100

    zadanie 3

--- hr.sql
+++ hr.sql
@@ -119,3 +119,49 @@ Kolumnom nadaj aliasy, a wynik posortuj w taki sposob by najpierw byl wiersze ma
 po nich te majace null w wartosci prowizji
 */
 
+select last_name nazwisko,commission_pct proporcja_prowizji,salary zarobki,salary*commission_pct wartosc_prowizji from employees
+order by commission_pct desc nulls last;
+
+--przerwa do 12:47
+
+select * from employees;
+
+select last_name,commission_pct,nvl(commission_pct,0) from employees;
+select last_name,commission_pct,nvl(commission_pct,0),salary*commission_pct,nvl(salary*commission_pct,0) from employees;
+
+select last_name,commission_pct,nvl(commission_pct,0),salary*commission_pct,nvl(salary*commission_pct,0) from employees order by 5 desc;
+
+select last_name,commission_pct,nvl(commission_pct,0) from employees order by nvl(commission_pct,0) desc;
+
+select * from employees;
+
+select * from employees where department_id=90;
+select * from employees where salary>5000;
+select * from employees where salary<5000;
+select * from employees where salary<=5000;
+select * from employees where salary>=5000;
+select * from employees where department_id<>90;
+select * from employees where department_id!=90;
+
+select * from employees where department_id=90 or department_id=40;
+select * from employees where department_id in (90,40,30);
+select * from employees where department_id not in (90,40,30);
+select * from employees where department_id=50 and salary>5000;
+select * from employees 
+where department_id=90 or department_id=40
+order by last_name;
+
+select * from employees where last_name like 'Kowalski';
+select * from employees where last_name='Kowalski';
+select * from employees where last_name like 'L%';
+select * from employees where last_name like 'l%';
+select * from employees where last_name like '%a%';
+select * from employees where last_name like '___a%';
+select * from employees where last_name not like '___a%';
+select * from employees where commission_pct=null; ---fuuuuuu
+select * from employees where commission_pct is null;
+select * from employees where commission_pct is not null;
+
+/*3.
+Wyœwietl nazwiska,imiona i wynagrodzenia osob zarabiajacych wiecej niz 10000. Wynik posortuj malej¹co wg. wynagrodzeñ.
+*/
