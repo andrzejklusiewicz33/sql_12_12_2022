e9ae8e27688151b4e8729bc372560d6cec735663
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-12 12:00:46 +0100

    zadanie 1

--- hr.sql
+++ hr.sql
@@ -1,1 +1,91 @@
 --test
+select * from employees;
+select * from jobs;
+select * from departments;
+select * from locations;
+select * from countries;
+select * from regions;
+select * from user_constraints;
+
+--DBEAVER
+
+select * from employees;
+select first_name,last_name,salary,department_id from employees;
+
+select department_id,employee_id,last_name from employees;
+
+
+select department_id numer_departamentu,employee_id "numer pracownika",last_name from employees;
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name from employees;
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by department_id;
+
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by department_id asc;
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by department_id desc;
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by numer_departamentu;
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by "numer pracownika";
+
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by "numer pracownika" desc;
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by 3;
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by 3 desc;
+
+
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by department_id,last_name;
+
+
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by 1,last_name;
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by 1 desc,last_name;
+
+
+
+select department_id as numer_departamentu,employee_id as "numer pracownika",last_name 
+from employees
+order by numer_departamentu desc,3;
+select * from employees;
+
+/*
+1. Wyœwietl nazwiska, daty zatrudnienia, wysokosci wynagrodzen w taki sposob by kazda z kolumn 
+miala alias a wynik byl posortowany malejaco wg. daty zatrudnienia a w przypadku tej samej daty zatrudnienia
+malejaco po wysokosci wynagrodzenia.
+*/
